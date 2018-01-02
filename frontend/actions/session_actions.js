a952029c5ba1256dda390d4002a2a46ee49e92f5
rossmcarthur <r.mcarth20@gmail.com>
2018-01-02 17:14:25 -0500

    Finishing Front-end User Auth

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,40 @@
+import * as APIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+export const receiveCurrentUser = currentUser => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+};
+
+export const receiveErrors = errors => {
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  };
+};
+
+export const signUp = user => dispatch => (
+  APIUtil.signUp(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), errors => (
+    dispatch(receiveErrors(errors.responseJSON))
+  ))
+);
+
+export const login = user => dispatch => (
+  APIUtil.login(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), errors => (
+    dispatch(receiveErrors(errors.responseJson))
+  ))
+);
+
+export const logout = () => dispatch => (
+  APIUtil.logout().then(user => (
+    dispatch(receiveCurrentUser(null))
+  ))
+);
