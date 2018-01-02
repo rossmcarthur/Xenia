8e6a50be673286c1835719513540269718ec1ee0
rossmcarthur <r.mcarth20@gmail.com>
2018-01-02 15:12:49 -0500

    backend auth complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root "static_pages#root"
+
+  namespace :api, defaults: {format: :json} do
+    resource :user, only: [:create, :show]
+    resource :session, only: [:create, :destroy, :show]
+  end
+
 end
