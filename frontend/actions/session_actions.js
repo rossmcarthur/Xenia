ab77ce5e30976ad9b2947fd8c06dcffd86d402a1
rossmcarthur <r.mcarth20@gmail.com>
2018-01-03 10:24:06 -0500

    Basic Working Login

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -3,15 +3,19 @@ import * as APIUtil from '../util/session_api_util'
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
-export const receiveCurrentUser = currentUser => ({
-  type: RECEIVE_CURRENT_USER,
-  currentUser
-});
+export const receiveCurrentUser = currentUser => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+};
 
-export const receiveErrors = errors => ({
-  type: RECEIVE_SESSION_ERRORS,
-  errors
-});
+export const receiveErrors = errors => {
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  };
+};
 
 export const signup = user => dispatch => (
   APIUtil.signup(user).then(user => (
