376cfcb351ee0628c972ac360851e1c6e7b30c15
rossmcarthur <r.mcarth20@gmail.com>
2018-01-03 09:40:44 -0500

    Correcting Greeting

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import SessionFormContainer from './session_form/session_form_container';
 import GreetingContainer from './greeting/greeting_container';
 const App = () => (
   <div>
     <header>
+      <Link to="/" className="header-link">
         <h1>Welcome To Xenia</h1>
+      </Link>
       <GreetingContainer />
     </header>
       <Route path="/login" component={SessionFormContainer} />
