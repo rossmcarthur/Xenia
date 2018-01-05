9e7e2d01ce9df11de9ded1f219ca77027c857d66
rossmcarthur <r.mcarth20@gmail.com>
2018-01-05 14:47:47 -0500

    Creating Map Components

--- frontend/components/spot_map/search_container.js
+++ frontend/components/spot_map/search_container.js
@@ -1,7 +1,6 @@
 import { connect } from 'react-redux';
 import { fetchSpots } from '../../actions/spot_actions';
-import SpotIndex from './spot_index';
-// import SpotMap from './spot_map/spot_map';
+import SpotIndex from '../spots/spot_index';
 
 const mapStateToProps = state => {
   return {
---
+++ frontend/components/spot_map/spot_map.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { withRouter } from 'react-router-dom';
+
+class SpotMap extends React.Component {
+
+  componentDidMount() {
+    const mapOptions = {
+      center: { lat: 37.7758, lng: -122.435 }, // this is SF
+      zoom: 13
+    };
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
+  }
+
+  render() {
+    return (
+      <div ref={ map => this.mapNode = map}></div>
+    );
+  }
+
+}
+
+export default SpotMap;
