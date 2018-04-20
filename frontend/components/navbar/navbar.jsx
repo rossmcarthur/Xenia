8dfa5dea883b090f653020f1f8ed5ccde001feb6
rossmcarthur <r.mcarth20@gmail.com>
2018-04-20 15:55:54 -0400

    Fixed navbar login-signup and map styling

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -8,9 +8,9 @@ class Navbar extends React.Component {
     if (this.props.loggedIn) {
       return (
       <div  className="nav-header">
-        <div className='nav-header-icon-container'>
+        <Link to='/' className='nav-header-icon-container'>
           <img className="nav-header-icon" src={window.staticImages.XeniaLogo}/>
-        </div>
+        </Link>
         <div>
           <SearchBarContainer />
         </div>
@@ -27,18 +27,20 @@ class Navbar extends React.Component {
     } else {
       return (
         <div  className="nav-header">
-          <Link to="/" className="nav-header-link">
+          <Link to="/" className="nav-header-icon-container">
             <img className="nav-header-icon" src={window.staticImages.XeniaLogo}/>
           </Link>
-            <div className="search-container">
+            <div >
               <SearchBarContainer />
             </div>
-            <button onClick={this.props.loginModal} className="nav-login-link">
-              <span className="button-border">Log In</span>
-            </button>
-          <button onClick={this.props.signupModal} className="nav-signup-link">
-            <span className="button-border">Sign Up</span>
-          </button>
+            <div className='nav-links'>
+              <button onClick={this.props.loginModal} className="nav-login-link">
+                <span className="button-border">Log In</span>
+              </button>
+              <button onClick={this.props.signupModal} className="nav-signup-link">
+                <span className="button-border">Sign Up</span>
+              </button>
+            </div>
           <SessionFormContainer />
         </div>
 
@@ -49,19 +51,3 @@ class Navbar extends React.Component {
 }
 
 export default Navbar;
-
-// <Link to="/" className="nav-header-link">
-//   <img className="nav-header-icon" src={window.staticImages.XeniaLogo}/>
-// </Link>
-//   <div className="search-container">
-//     <SearchBarContainer />
-//   </div>
-//   <div className="nav-links">
-//       <img className="nav-user-image" src={this.props.currentUser.image_url}/>
-//       <Link to="/">
-//         <button className="logout-button" onClick={this.props.logout}>Log Out</button>
-//       </Link>
-//       <Link to="/spots/create">
-//         <button className="host-button">Become a host</button>
-//       </Link>
-//   </div>
